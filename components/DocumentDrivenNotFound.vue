73b1d59ca45706b7892eea84d50eecb4285911ea
Chloe <cguttweb@pm.me>
2022-11-28 21:39:45 +0000

    added some components and base layout

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Page not found</h1>
+  <p>Back to <nuxt-link to="/">home</nuxt-link></p>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-yellow-700 h-screen">
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
